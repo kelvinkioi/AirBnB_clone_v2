fafc41dea9ec31315154ea1b1ecbad2aaa003732
fahrenh8T <raymondrayn98@gmail.com>
2023-09-19 21:32:44 +0300

    MySQL setup test; setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- script prepares a MySQL Test server for the project.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant privileges to hbnb_dev user
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+-- ensures user has the required privileges
+FLUSH PRIVILEGES;
