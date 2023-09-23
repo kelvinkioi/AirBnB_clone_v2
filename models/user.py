3f0aff96e0880642d57dd70b64ce8efc1c58979a
fahrenh8T <raymondrayn98@gmail.com>
2023-09-24 00:04:57 +0300

    user.py updt

--- models/user.py
+++ models/user.py
@@ -8,12 +8,16 @@ import os
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    if storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship('Place', cascade="all,delete", backref='user')
+        reviews = relationship('Review', cascade="all,delete", backref='user')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
