0fd66635612a19968f3914cdbe59ef00c95995b2
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-10-20 11:11:49 -0700

    display a HTML page only if n is an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application.
+"""
+from flask import Flask, render_template
+import re
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    display “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+     display “C ” followed by the value of the text variable
+     and replace underscore _ symbols with a space
+    """
+    new_text = re.sub(r"_", " ", text)
+    return f"C {new_text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    display “Python ”, followed by the value of the text variable
+    and replace underscore _ symbols with a space
+    The default value of text is “is cool”
+    """
+    new_text = re.sub(r"_", " ", text)
+    return f"Python {new_text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    display “n is a number” only if n is an integer
+    """
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """
+    display a HTML page only if n is an integer
+    H1 tag: “Number: n” inside the tag BODY
+    """
+    return render_template("5-number.html", num = n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
