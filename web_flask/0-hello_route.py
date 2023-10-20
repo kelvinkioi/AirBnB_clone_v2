d9015856356ffd45d9d438f3ddab64ebaf053a6a
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-10-20 01:55:24 -0700

    added what ip and port to listen on for the web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,9 @@ script that starts a Flask web application
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello():
     """
     display “Hello HBNB!”
@@ -17,4 +16,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
