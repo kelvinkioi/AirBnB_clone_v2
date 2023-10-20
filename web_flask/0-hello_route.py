3ec8ee259d93f0d6cf688275281c293ed3c15be8
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-10-20 01:47:31 -0700

    script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """
+    display “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run()
